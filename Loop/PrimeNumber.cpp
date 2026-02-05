d4c5f48401cce456e0c319b265aad47d09e6aa1c
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-05 09:26:36 +0530

    .

---
+++ Loop/PrimeNumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Prime numbers up to " << n << " are:" << endl;
+    for (int num = 2; num <= n; num++) {
+        bool isPrime = true;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            cout << num << " ";
+        }
+    }
+}
