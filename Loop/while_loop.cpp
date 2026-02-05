c5fffaaa4063950a41027a49b20a63192cbfd201
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-05 09:20:53 +0530

    .

---
+++ Loop/while_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+int n,count=1;
+cout <<"Enter the number :";
+cin>> n;
+while(count<= n){
+cout<<count<<" ";
+count++;
+}
+cout<<endl;
+    return 0;
+}
