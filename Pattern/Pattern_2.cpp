e8379dca5674d9030a56569b5c01b79974a8be6f
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-06 09:09:51 +0530

    .

---
+++ Pattern/Pattern_2.cpp
@@ -0,0 +1,21 @@
+    // A B C D 
+    // A B C D 
+    // A B C D 
+    // A B C D 
+#include <iostream>
+using namespace std;
+int main(){
+    int n=4;
+for(int i=0;i<n ;i++){
+    int m=4;
+    char ch='A';
+    for(int j=0; j<m;j++){
+        cout<<ch<<" ";
+        ch+=1;
+    }
+    cout<<endl;
+}
+
+
+    return 0;
+}
