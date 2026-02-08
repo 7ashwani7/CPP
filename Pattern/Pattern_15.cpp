ec211e0b00ea2bcf545a1fd9bef152a13c127804
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-08 17:00:49 +0530

    .

---
+++ Pattern/Pattern_15.cpp
@@ -0,0 +1,31 @@
+//  Solid Rhombus
+//         * * * * * 
+//       * * * * * 
+//     * * * * * 
+//   * * * * * 
+// * * * * * 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        // Print stars
+        for (int k = 1; k <= n; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
