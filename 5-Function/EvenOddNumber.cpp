faac8e482db8dbdaca11aec871fd1fc185c7547a
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-10 09:05:30 +0530

    .

---
+++ 5-Function/EvenOddNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+bool isEven(int number) {
+    return number % 2 == 0;
+}
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (isEven(num)) {
+        cout << num << " is an even number." << endl;
+    } else {
+        cout << num << " is an odd number." << endl;
+    }
+    return 0;
+}
