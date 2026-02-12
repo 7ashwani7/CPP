789ea7dfb123c96260b495f7fb26b55b917c45a3
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-12 18:12:58 +0530

    .

---
+++ 7-2DArray/Matrix.cpp
@@ -0,0 +1,30 @@
+// Taking a matrix as an input and printing its elements.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    int matrix[rows][cols];
+
+    cout << "Enter the elements of the matrix: " << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "The elements of the matrix are: " << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
