0a6eafc7dc9fbf4d02b425a79f0ee1b4fadd47b7
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-12 18:13:27 +0530

    .

---
+++ 7-2DArray/Transpose.cpp
@@ -0,0 +1,30 @@
+// For a given matrix of N x M, print its transpose.
+#include <iostream>
+using namespace std;
+
+int main() {
+int rows, cols;
+cout << "Enter the number of rows: ";
+cin >> rows;
+cout << "Enter the number of columns: ";
+cin >> cols;
+
+int matrix[rows][cols];
+
+cout << "Enter the elements of the matrix: " << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "The transpose of the matrix is: " << endl;
+    for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
