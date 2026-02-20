863a07c84e07a2277b4b3cf7e9d516f4d26db04b
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-20 10:05:54 +0530

    .

---
+++ 11-Searching/LinearSearch.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Enter the target element to search: ";
+    int target;
+    cin >> target;
+
+    int result = linearSearch(arr,n,target);
+    
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found in the array." << endl;
+    }
+    
+    return 0;
+}
