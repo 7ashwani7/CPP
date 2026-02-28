3873ea117713ad3089ba5033f4d2c36059a797f4
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-28 17:25:58 +0530

    .

---
+++ 13-Recursion/PrintNTo1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void printNumber(int n) {
+if (n == 0) {
+    return;
+}
+    cout << n << endl;
+    printNumber(n - 1);
+    
+    
+}
+int main() {
+    int n;
+    cout << "Enter a number: ";   
+    cin >> n;
+    printNumber(n);
+    return 0;
+}
